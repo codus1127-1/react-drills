5bec2a0e11b23213627ebfb56999e301db5d4918
Cody Young <codyjamesyoung@gmail.com>
2019-10-14 18:26:00 -0600

    complete

--- app-8/src/App.js
+++ app-8/src/App.js
@@ -1,18 +1,46 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import axios from 'axios'
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state ={
+      lukeSkywalker: '',
+      homeWorld: ''
+    }
+  }
+
+  componentDidMount(){
+    axios.get('https://swapi.co/api/people/10')
+    .then(response => {
+      this.setState({
+        lukeSkywalker: response.data
+      })
+    })
+  
+  }
+
+  // componentDidUpdate(){
+  //   axios.get("https://swapi.co/api/planets/20/")
+  //   .then(response => {
+  //     this.setState({
+  //       homeWorld: response.data
+  //     })
+  //   })
+  // }
+
   render() {
+    console.log(this.state.lukeSkywalker)
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Name: {this.state.lukeSkywalker.name} </h1>
+        <h1>Birth Year: {this.state.lukeSkywalker.birth_year} </h1>
+        <h1>Height: {this.state.lukeSkywalker.height} cm</h1>
+        <h1>Eye Color: {this.state.lukeSkywalker.eye_color} </h1>
+        <h1>Hair Color: {this.state.lukeSkywalker.hair_color}</h1>
+        {/* <h1>Home World: </h1> */}
+       
       </div>
     );
   }
